42bc07d29cac17629b44bf0a1725f6ab20b2c55f
Lingyu Zhou <zly@DebianPioneer.home>
2019-04-30 10:18:43 -0400

    Updated arch, separate sinks and sources

--- ibus-proxy.py
+++ ibus-proxy.py
@@ -4,50 +4,44 @@ import asyncio
 sources = []
 sinks = []
 
-async def echo_server(reader, writer):
-  if len(clients)==0:
-    await input_server(reader, writer)
-  else:
-    await output_server(reader, writer)
-
-async def input_server(reader, writer):
+async def source_server(reader, writer):
   peerAddr = writer.get_extra_info("socket").getpeername()[0]
   print("Adding {}".format(peerAddr))
-  print("# Current client number: {}".format(len(clients)))
-  clients.append(writer)
+  print("# Current source number: {}".format(len(sources)))
+  sources.append(writer)
   while True:
     data = await reader.read(100)  # Max number of bytes to read
     if not data:
       break
-    for curWriter in clients:
+    for curWriter in sinks:
       if writer == curWriter:
         continue
       curWriter.write("{}\t{}".format(peerAddr,data.decode().replace('\t','\t\t')) \
                               .encode()
                       )
-    await asyncio.wait([writer.drain() for writer in clients])  # Flow control, see later
-  clients.remove(writer)
+    await asyncio.wait([writer.drain() for writer in sources])  # Flow control, see later
+  sources.remove(writer)
   print("Removed {}".format(peerAddr))
-  print("# Current client number: {}".format(len(clients)))
+  print("# Current client number: {}".format(len(sources)))
   writer.close()
 
-async def output_server(reader, writer):
+async def sink_server(reader, writer):
   peerAddr = writer.get_extra_info("socket").getpeername()[0]
   print("Adding {}".format(peerAddr))
-  print("# Current client number: {}".format(len(clients)))
-  clients.append(writer)
+  print("# Current client number: {}".format(len(sinks)))
+  sinks.append(writer)
   while True:
     data = await reader.read(100)  # Max number of bytes to read
     if not data:
       break
-  clients.remove(writer)
+  sinks.remove(writer)
   print("Removed {}".format(peerAddr))
-  print("# Current client number: {}".format(len(clients)))
+  print("# Current client number: {}".format(len(sinks)))
   writer.close()
 
-async def main(host, portIn):
-  server = await asyncio.start_server(input_server, host, portIn)
-  server = await asyncio.start_server(output_server, host, portIn)
-  await server.serve_forever()
+async def main(host, portIn, portOut):
+  server1 = await asyncio.start_server(source_server, host, portIn)
+  server2 = await asyncio.start_server(sink_server, host, portOut)
+  await asyncio.wait([server1.serve_forever(),server2.serve_forever()])
 
-asyncio.run(main('127.0.0.1', 5000))
+asyncio.run(main('0.0.0.0',9001,9000))
