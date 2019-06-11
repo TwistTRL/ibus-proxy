786e00a686855540d89f2685f4573cad1cd169b5
zhoulytwinyu <zhoulytwin@gmail.com>
2019-06-11 11:37:29 -0400

    rename

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='ibus_proxy',
+    name='ibus-proxy',
     version='0.1.0',
     author='Lingyu Zhou',
     author_email='zhoulytwin@gmail.com',
-    scripts=['bin/ibus_proxy.py'],
-    url='https://github.com/TwistTRL/ibus_proxy',
+    scripts=['bin/ibus-proxy.py'],
+    url='https://github.com/TwistTRL/ibus-proxy',
     license='GPL-3.0',
-    description='https://github.com/TwistTRL/ibus_proxy',
+    description='https://github.com/TwistTRL/ibus-proxy',
     install_requires=[
         "docopt >= 0.6.1",
     ],
